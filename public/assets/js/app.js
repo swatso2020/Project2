39cc18c69d13f64a2daf22b5adbf4db902201e35
marco p <miamarine84@yahoo.com>
2020-10-06 18:56:20 -0400

    ajax working

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,4 +1,15 @@
-$("#meal-search").click(function(e){
-    e.preventDefault();
-    console.log("testing")
+$("#meal-search").click(function (e) {
+  e.preventDefault();
+  let userInput = $("#fname").val();
+  ajaxCallSearch(userInput);
+});
+
+
+function ajaxCallSearch(userInput) {
+  let ajaxCall = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${userInput}`
+  $.ajax({
+    url: ajaxCall, success: function (result) {
+      
+    }
   });
+}
