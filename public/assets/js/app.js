cf2421ab4dd5ddef93f1debb2bcf30ad05f80f0b
marco p <miamarine84@yahoo.com>
2020-10-06 20:24:25 -0400

    ajax working and getting 3 random searches by users choice of protein

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,6 +1,6 @@
-$("#meal-search").click(function (e) {
+$("#search").click(function (e) {
   e.preventDefault();
-  let userInput = $("#fname").val();
+  let userInput = $("#search-input").val();
   ajaxCallSearch(userInput);
 });
 
@@ -8,8 +8,19 @@ $("#meal-search").click(function (e) {
 function ajaxCallSearch(userInput) {
   let ajaxCall = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${userInput}`
   $.ajax({
+
     url: ajaxCall, success: function (result) {
-      
+      result = JSON.stringify(result)
+      result = JSON.parse(result)
+      console.log(result)
+      for(let j = 0;j < 3; j++) {
+        console.log(result.meals[Math.floor(Math.random() * 11)], "line 16");
+        
+        // console.log(result.meals[Math.floor(Math.random() * 11)].strMeal, "IdMeal")
     }
+
+
+  }
   });
+
 }
