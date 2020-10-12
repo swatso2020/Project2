8bd39b6dee15854b99b836d4d3a9170e482de798
Sean Watson <swatso2020@gmail.com>
2020-10-12 18:37:34 -0400

    getting review to the database

--- db/schema.sql
+++ db/schema.sql
@@ -1,17 +1,17 @@
-
+--creating whatscookin database--
 DROP DATABASE IF EXISTS whatsCookin_db;
-
 CREATE DATABASE whatsCookin_db;
 
 
-
-
-
-
-
-
---see everything in the favorite meals table
+         --Database Queries--
+-- see everything in the favorite meals table
 select *
 from whatscookin_db.FavoriteMeals
+
+select *
+from whatscookin_db.Ingridents
+
+
 --drop favorite meal table
+drop table whatscookin_db.favoriteMeals;
 drop table whatscookin_db.favoriteMeals;
