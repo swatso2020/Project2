31a84251291c925d3c432822f930a0d0e4d96648
marco p <miamarine84@yahoo.com>
2020-10-13 18:31:04 -0400

    merging passport example with our files

--- models/index.js
+++ models/index.js
@@ -25,6 +25,10 @@ fs
   .filter(file => {
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
+  // .forEach(function(file) {
+  //   var model = sequelize['import'](path.join(__dirname, file));
+  //   db[model.name] = model;
+  // });
   .forEach(file => {
     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
