e18b3e40f7c232dcae03f5a0038a875dd1e0ab76
Caitlin White <caitlinritawhite@gmail.com>
2020-10-14 21:16:59 -0400

    favorites button and review form edits

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -3,6 +3,13 @@ console.log('works')
 $("#search").click(function (event) {
   event.preventDefault();
   let userInput = $("#search-input").val();
+  document.getElementById("recipeList").style.display="block";
+  document.getElementById("favorite-results").style.display="none";
+  document.getElementById("review-text").style.display="none";
+    document.getElementById("faves").style.display="inline";
+    document.getElementById("add").style.display="inline";
+     document.getElementById("add").style.display="inline";
+ 
   ajaxCallSearch(userInput);
 });
 
@@ -68,6 +75,18 @@ function ajaxCallRecipe(mealid) {
         .attr("src", result.meals[0].strMealThumb).addClass("food-image");
       document.getElementById("recipeDetail").style.display = "block";
       document.getElementById("review").style.display = "inline";
+
+
+      //hiding and showing review, favorites and add recipe data
+   
+      document.getElementById("submit-btn").style.display = "none";
+      document.getElementById("faves").style.display = "inline";
+      document.getElementById("add").style.display = "inline";
+      document.getElementById("review-text").style.display="none";
+      
+      
+
+
       $("#image-div").append(foodImg);
       $("#recipeTitle").html(result.meals[0].strMeal);
       $("#instructions").html(result.meals[0].strInstructions);
@@ -156,6 +175,15 @@ function randomRecipe(mealid) {
 
       $("#instructions").html(result.meals[0].strInstructions);
 
+        //hiding and showing review, favorites and add recipe data
+   
+        document.getElementById("submit-btn").style.display = "none";
+        document.getElementById("faves").style.display = "inline";
+        document.getElementById("add").style.display = "inline";
+        document.getElementById("review-text").style.display="none";
+      
+    
+
       const ingredients = [];
       for (let i = 1; i <= 20; i++) {
 
@@ -228,9 +256,14 @@ function saveRecipe() {
 
   $("#review").click(function (event) {
     event.preventDefault();
+    document.getElementById("submit-btn").style.display = "inline";
+    document.getElementById("review-input").style.display="inline";
     document.getElementById("review-form").style.display = "block";
     document.getElementById("review").style.display = "none";
     document.getElementById("faves").style.display= "none";
+    document.getElementById("add").style.display= "none";
+  
+  
    
   });
   
@@ -248,4 +281,14 @@ function saveRecipe() {
 
     
 
+  })
+
+  $("#faves").click(function(event){
+    event.preventDefault();
+    
+    document.getElementById("favorite-results").style.display="inline";
+    document.getElementById("recipeList").style.display="none";
+    document.getElementById("recipeDetail").style.display="none";
+    document.getElementById("review").style.display="none";
+    document.getElementById("faves").style.display="none";
   })
